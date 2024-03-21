b105748ea9fb37efb01b18f1496e33961d4e8f13
Padin11 <92806667+Padin11@users.noreply.github.com>
2024-03-21 11:29:07 -0300

    1.0
    Primeira versão do código, conta com alguns erros.

--- POO/Lista1/Questao1.cpp
+++ POO/Lista1/Questao1.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Agenda{
+private:
+  string nome;
+  string telefone;
+
+
+public:
+  void setNome(string nome1){
+    this->nome = nome1;
+  }
+
+string getNome(){
+  return this->nome;
+}
+void setTelefone(string telefone1){
+    this->nome = telefone1;
+  }
+
+string getTelefone(){
+  return this->telefone;
+}
+};
+int main() {
+  string x,y;
+  cout << "Coloque seu nome: "<<endl;
+  cin>>y;
+  cout <<"Coloque seu telefone: "<< endl;
+  cin>>x;
+  Agenda a1;
+  a1.setNome(y);
+  a1.setTelefone(x);
+  cout<<"Nome: "<<a1.getNome()<<endl;
+  cout<<"Telefone: "<<a1.getTelefone();
+ return 0;
+}
